b749e713fb857cd5f3ef9a39f858578a60c9dd19
Tereza Smutna <terka.kop@seznam.cz>
2022-11-28 14:30:50 +0100

    spravna URL a KEY

--- src/functions/supabase.js
+++ src/functions/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://roataydfibvrvlnjukhw.supabase.co';
+const SUPABASE_URL = 'https://vebgpzeowgiaxsncxujh.supabase.co';
 const SUPABASE_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvYXRheWRmaWJ2cnZsbmp1a2h3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjkxMzY5NDksImV4cCI6MTk4NDcxMjk0OX0.NcwW19bzCQPw49vKM-yIowj4JP4JvsPSA4DMvw0AvbM';
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlYmdwemVvd2dpYXhzbmN4dWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjkxMTkyNzQsImV4cCI6MTk4NDY5NTI3NH0.MQnIW-35wM5MaZ63u-zC-b_7senMs7LDGojXkBvFhQ8';
 
 export const getSupabase = () => {
   return createClient(SUPABASE_URL, SUPABASE_KEY);
