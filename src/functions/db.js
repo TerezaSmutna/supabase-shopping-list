9bda5e972a47d08b611c433884a1dc4be93fc16f
Tereza Smutna <terka.kop@seznam.cz>
2022-11-28 19:12:59 +0100

    supabase.js and db.js corrected

--- src/functions/db.js
+++ src/functions/db.js
@@ -3,12 +3,12 @@ import { getSupabase } from './supabase.js';
 export const getShoppingItems = (userId) => {
   const supabase = getSupabase();
 
-  return supabase.from('shopping_items').select('*').eq('user_id', userId);
+  return supabase.from('shopping_item').select('*').eq('user_id', userId);
 };
 
 export const addShippingItem = (product, amount, unit, userId) => {
   const supabase = getSupabase();
-  return supabase.from('shopping_items').insert({
+  return supabase.from('shopping_item').insert({
     product: product,
     amount: amount,
     unit: unit,
--- src/functions/supabase.js
+++ src/functions/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://vebgpzeowgiaxsncxujh.supabase.co';
 const SUPABASE_KEY =
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlYmdwemVvd2dpYXhzbmN4dWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjkxMTkyNzQsImV4cCI6MTk4NDY5NTI3NH0.MQnIW-35wM5MaZ63u-zC-b_7senMs7LDGojXkBvFhQ8';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlYmdwemVvd2dpYXhzbmN4dWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjkxMTkyNzQsImV4cCI6MTk4NDY5NTI3NH0.MQnIW-35wM5MaZ63u-zC-b_7senMs7LDGojXkBvFhQ8';
 
 export const getSupabase = () => {
   return createClient(SUPABASE_URL, SUPABASE_KEY);
